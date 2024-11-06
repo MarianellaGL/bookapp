47f5b7f6bda47b2e0768d81611ac2fb206d207f2
marianellagl <Marianella.gomezluna@gmail.com>
2024-11-06 20:44:02 -0300

    app inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ButtonAppBar from './Components/ButtonAppBar/ButtonAppBar'
+
+function App() {
+
+
+
+  return (
+    <>
+    <ButtonAppBar/>
+ </>
+  )
+}
+
+export default App
